06304ef8f82eba2e87ca8911e759afd82ef70393
Sameer Sharif Chauhan <119564165+Sameer12223044@users.noreply.github.com>
2025-03-23 14:34:53 +0530

    23 March

---
+++ Array/Majority Element.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int majorityElement(vector<int>& nums){
+    sort(nums.begin(),nums.end());
+    return nums[nums.size()/2];
+}
+int main(){
+    vector<int>nums={2,2,1,1,1,2,2};
+    int result=majorityElement(nums);
+    cout<<"Majority Element: " <<result<<endl;
+    return 0;
+}
