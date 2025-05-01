399b536915ea890bbb1529d80c30262ce71902b2
Sameer Sharif Chauhan <119564165+Sameer12223044@users.noreply.github.com>
2025-05-01 10:34:40 +0530

    1 May

---
+++ Leetcode/1423. Maximum Points You Can Obtain from Cards.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n=cardPoints.size();
+        int total=0;
+        for(int point:cardPoints){
+            total+=point;
+        }
+        int windowSize=n-k;
+        int windowSum=0;
+        for(int i=0;i<windowSize;++i){
+            windowSum+=cardPoints[i];
+        }
+        int minWindowSum=windowSum;
+        for (int i=windowSize;i<n;++i){
+            windowSum+=cardPoints[i]-cardPoints[i-windowSize];
+            minWindowSum=min(minWindowSum,windowSum);
+        }
+        return total-minWindowSum;
+    }
+};
+
